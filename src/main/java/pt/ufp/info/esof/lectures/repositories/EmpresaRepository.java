3082bad2567b93dec40e8c694eb563f5b8ce6bc0
andre <35604@ufp.edu.pt>
2020-12-09 00:19:43 +0000

    Classes e metodos implementados

--- src/main/java/pt/ufp/info/esof/lectures/repositories/EmpresaRepository.java
+++
@@ -1,9 +0,0 @@
-package pt.ufp.info.esof.lectures.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import pt.ufp.info.esof.lectures.models.Empresa;
-
-@Repository
-public interface EmpresaRepository extends CrudRepository<Empresa,Long> {
-}
---
+++ src/test/java/pt/ufp/inf/esof/projeto/models/FuncionarioTest.java
@@ -0,0 +1,27 @@
+package pt.ufp.inf.esof.projeto.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FuncionarioTest {
+
+    @Test
+    void getValorHora() {
+
+    }
+
+    @Test
+    void consultarTarefasNaoConcluidas() {
+
+    }
+
+    @Test
+    void ditarTempo() {
+
+    }
+
+    @Test
+    void atualizaPercentagemTarefa() {
+    }
+}
