4e6a89509afe31c9a610ccc219130bc77d20b247
Brumix2000 <brunompereira42@gmail.com>
2021-01-01 16:56:15 +0000

    safe

---
+++ src/main/java/EngSoftProjeto/dtos/IdDTO.java
@@ -0,0 +1,14 @@
+package EngSoftProjeto.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IdDTO {
+    private float ID;
+}
