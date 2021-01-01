4d172fe06361f2ce19248191f079ce6ad2ec873e
Brumix2000 <brunompereira42@gmail.com>
2021-01-01 19:54:24 +0000

    refactor codacy

--- src/test/java/pt/ufp/inf/esof/projeto/ProjetoApplicationTests.java
+++ src/test/java/pt/ufp/inf/esof/projeto/ProjetoApplicationTests.java
@@ -1,10 +1,8 @@
 package pt.ufp.inf.esof.projeto;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProjetoApplicationTests {
-
 
 }
